d21835715ee896686200ee38dc81955ee5c86e29
Nikhil5858 <123rathodnikhil@gmail.com>
2025-06-29 12:17:25 +0530

    Login page

---
+++ HMS/Controllers/AdminController.cs
@@ -0,0 +1,36 @@
+﻿using HMS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+
+namespace HMS.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AdminDashboard()
+        {
+            return View();
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(string email,string password)
+        {
+            DatabaseMethod databasemethod = new DatabaseMethod();
+            SqlDataReader result = databasemethod.check_Login(email, password);
+            if (result.HasRows)
+            {
+                return View("AdminDashboard");
+            }
+            else {
+                TempData["Message"] = "Invallid User!";
+                return View();
+            }
+        }
+    }
+}
---
+++ HMS/Models/DatabaseMethod.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Data.SqlClient;
+using Microsoft.Data;
+using System.Data;
+ 
+
+namespace HMS.Models
+{
+    public class DatabaseMethod
+    {
+        string connection = "Data Source=DESKTOP-A45M567\\SQLEXPRESS01;Initial Catalog=HMS;Integrated Security=True;Encrypt=False";
+
+        SqlConnection? con = null;
+
+        public DatabaseMethod()
+        {
+            con = new SqlConnection(connection);
+        }
+
+        public SqlDataReader check_Login(string email, string password)
+        {
+            string query = "select Email,Password from [User] where Email=@email And Password=@password";
+            SqlCommand sqlCommand = new SqlCommand(query, con);
+            sqlCommand.CommandType = CommandType.Text;
+            sqlCommand.Parameters.AddWithValue("@email", email);
+            sqlCommand.Parameters.AddWithValue("@password", password);
+            if (con.State != ConnectionState.Open)
+            {
+                con.Open();
+            }
+            SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+            return sqlDataReader;
+        }
+
+
+
+    }
+}
