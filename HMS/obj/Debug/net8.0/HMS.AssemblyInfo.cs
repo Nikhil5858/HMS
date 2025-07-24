42e25a44f5bdad6e17ca4b6de199c2ba3b1a7cef
Nikhil5858 <123rathodnikhil@gmail.com>
2025-07-24 19:03:39 +0530

    update the cancel button in all components

--- HMS/obj/Debug/net8.0/HMS.AssemblyInfo.cs
+++ HMS/obj/Debug/net8.0/HMS.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HMS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3e1f1982b7a23fe4298c890050efce90bd058a4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+70ea26502cf698002627ca511b8bf0b3e397ecce")]
 [assembly: System.Reflection.AssemblyProductAttribute("HMS")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HMS")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
